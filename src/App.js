cf8fcfc17cc589ce74168d47356680f5a2be085b
Dmitry Mylnikov <dmitry.mylnikov@fusion-team.com>
2019-09-24 18:06:08 +0300

    Finished app

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from "@material-ui/styles";
+import RGL, { WidthProvider } from 'react-grid-layout';
+
+
+import "./App.css";
+import "../node_modules/react-grid-layout/css/styles.css";
+import "../node_modules/react-resizable/css/styles.css";
+
+const ReactGridLayout = WidthProvider(RGL);
+
+const useStyles = makeStyles({
+  square: {
+    backgroundColor: '#1976d2',
+  },
+  rectangle: {
+    backgroundColor: 'red',    
+  }
+});
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const classes = useStyles();
+
+  const layout = [
+    {i: 'a', x: 0, y: 0, w: 1, h: 1, maxW: 1, maxH: 1 },
+    {i: 'b', x: 1, y: 0, w: 2, h: 1, maxW: 5, maxH: 1},
+  ]
+
+  return <div className="App">
+    <ReactGridLayout verticalCompact={false} layout={layout} >
+
+    <Card key="a" className={classes.square}>
+      <CardContent>
+        <Typography>
+          The square card
+        </Typography>
+      </CardContent>
+    </Card>
+    <Card key="b" className={classes.rectangle}>
+      <CardContent>
+        <Typography>
+          The rectangular card
+        </Typography>
+      </CardContent>
+    </Card>
+    </ReactGridLayout>
+  </div>;
 }
 
 export default App;
